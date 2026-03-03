3cb02f03f6b2f44e9e4983629e9fd30267ada850
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-03-03 09:52:26 -0300

    Primeiro componente

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -3,6 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// no react, componentes são FUNÇÕES
+
+function FormularioDeEvento () {
+  return (
+    <form className="form-evento">
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="">
+          Qual o nome do evento?
+        </label>
+        <input type="text" id="nome"/>
+      </fieldset>
+    </form>
+  )
+}
+
 function App() {
   return (
     <main>
@@ -12,6 +28,7 @@ function App() {
       <section>
         <img src="/banner.png" alt="banner"/>
       </section>
+      <FormularioDeEvento/>
     </main>
   )
 }
