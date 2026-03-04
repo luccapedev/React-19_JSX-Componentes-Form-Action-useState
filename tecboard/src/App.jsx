c9e3e3ebdef69843ac660ae2f1d6c5dca48ee424
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-03-04 09:35:16 -0300

    Passagem de parâmetros

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
+import { Children, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 // no react, componentes são FUNÇÕES
 
+function TituloFormulario (props) {
+  return (
+    <h2>{props.children}</h2>
+  )
+}
+
 function FormularioDeEvento () {
   return (
     <form className="form-evento">
-      <h2>Preencha para criar um evento:</h2>
+      <TituloFormulario>
+        Preencha para criar um evento
+      </TituloFormulario>
       <fieldset>
         <label htmlFor="">
           Qual o nome do evento?
         </label>
-        <input type="text" id="nome"/>
+        <input type="text" id="nome" placeholder='Summer dev hits'/>
       </fieldset>
     </form>
   )
