c5b64a902accea573172d3db54a6e0d57fbf3e5f
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-03-09 11:46:10 -0300

    Finalizando o formulário

--- tecboard/src/componentes/FormularioDeEvento/index.jsx
+++ tecboard/src/componentes/FormularioDeEvento/index.jsx
@@ -6,7 +6,7 @@ import { TituloFormulario } from "../TituloFormulario";
 import { Botao } from '../Botao';
 import { ListaSuspensa } from '../ListaSuspensa';
 
-export function FormularioDeEvento () {
+export function FormularioDeEvento ({ temas }) {
   return (
     <form className="form-evento">
       <TituloFormulario>
@@ -24,6 +24,16 @@ export function FormularioDeEvento () {
             name='nomeEvento'/>
         </CampoDeFormulario>
         <CampoDeFormulario>
+          <Label htmlFor="capa">
+            Qual o endereço da imagem de capa?
+          </Label>
+          <CampoDeEntrada 
+            type="text" 
+            id="capa" 
+            placeholder='http://...'
+            name='capa'/>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
           <Label htmlFor="dataEvento">
             Data do evento
           </Label>
@@ -35,10 +45,10 @@ export function FormularioDeEvento () {
         </CampoDeFormulario>
 
         <CampoDeFormulario>
-          <Label htmlFor="dataEvento">
-            Data do evento
+          <Label htmlFor="tema">
+            Tema do evento
           </Label>
-          <ListaSuspensa/>
+          <ListaSuspensa id ="tema" name="tema" itens={temas}/>
         </CampoDeFormulario>
       </div>
       <div className='acoes'>
