6ac0c21108868495c660bf4da058b1cca61155ef
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-03-09 10:16:31 -0300

    Iterando por uma lista

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -5,58 +5,62 @@ import { Tema } from './componentes/Tema'
 
 function App() {
 
-const temas = [
-  {
-    id: 1,
-    nome: 'front-end'
-  },
-  {
-    id: 2,
-    nome: 'back-end'
-  },
-  {
-    id: 3,
-    nome: 'devops'
-  },
-  {
-    id: 4,
-    nome: 'inteligência artificial'
-  },
-  {
-    id: 5,
-    nome: 'data science'
-  },
-  {
-    id: 6,
-    nome: 'cloud'
-  },
-]
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    },
+  ]
 
   return (
     <main>
       <header>
-        <img src="/logo.png" alt="logo"/>
+        <img src="/logo.png" alt="logo" />
       </header>
-      <Banner/>
-      <FormularioDeEvento/>
-      <section>
-        <Tema tema={temas[0]}/>
+      <Banner />
+      <FormularioDeEvento />
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+          </section>
+        )
+      })}
+{/*       <section>
+        <Tema tema={temas[1]} />
       </section>
       <section>
-        <Tema tema={temas[1]}/>
+        <Tema tema={temas[2]} />
       </section>
       <section>
-        <Tema tema={temas[2]}/>
+        <Tema tema={temas[3]} />
       </section>
       <section>
-        <Tema tema={temas[3]}/>
+        <Tema tema={temas[4]} />
       </section>
       <section>
-        <Tema tema={temas[4]}/>
-      </section>
-      <section>
-        <Tema tema={temas[5]}/>
-      </section>
+        <Tema tema={temas[5]} />
+      </section> */}
     </main>
   )
 }
