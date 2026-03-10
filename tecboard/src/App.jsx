03119cf8f495752ac345ce647fa153b1a18210ea
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-03-10 11:44:44 -0300

    Grid de eventos

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -54,16 +54,20 @@ function App() {
       </header>
       <Banner />
       <FormularioDeEvento temas={temas} aoSubmeter={adicionarEvento} />
-      {temas.map(function (item) {
-        return (
-          <section key={item.id}>
-            <Tema tema={item} />
-            {eventos.map(function (item, indice) {
-            return <CardEvento evento={item} key={indice}/>
-            })}
-          </section>
-        )
-      })}
+      <section className="container">
+        {temas.map(function (item) {
+          return (
+            <section key={item.id}>
+              <Tema tema={item} />
+              <div className="eventos">
+              {eventos.map(function (item, indice) {
+                return <CardEvento evento={item} key={indice} />
+              })}
+              </div>
+            </section>
+          )
+        })}
+      </section>
     </main>
   )
 }
