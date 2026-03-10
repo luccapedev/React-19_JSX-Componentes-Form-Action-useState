b85b0a7f1c11afbbb5924f9f3749f25b2df94c91
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-03-10 11:55:00 -0300

    Renderização condicional

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -55,14 +55,23 @@ function App() {
       <Banner />
       <FormularioDeEvento temas={temas} aoSubmeter={adicionarEvento} />
       <section className="container">
-        {temas.map(function (item) {
+        {temas.map(function (tema) {
+          if (!eventos.some(function (evento) {
+            return evento.tema.id == tema.id
+          })) {
+            return null
+          }
           return (
-            <section key={item.id}>
-              <Tema tema={item} />
+            <section key={tema.id}>
+              <Tema tema={tema} />
               <div className="eventos">
-              {eventos.map(function (item, indice) {
-                return <CardEvento evento={item} key={indice} />
-              })}
+                {eventos.
+                  filter(function (evento) {
+                    return evento.tema.id == tema.id
+                  })
+                  .map(function (evento, indice) {
+                    return <CardEvento evento={evento} key={indice} />
+                  })}
               </div>
             </section>
           )
